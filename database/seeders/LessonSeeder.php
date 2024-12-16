5c09e63ea3347cc2622ced74f76bf613bd7c78ce
patrickrayy <raynerpatrick4@gmail.com>
2024-12-16 13:05:39 +0700

    .2

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Lesson;
 use App\Models\KategoriLesson;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class LessonSeeder extends Seeder
 {
@@ -13,20 +14,34 @@ class LessonSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
+
+        // Fungsi untuk mengambil gambar dari Picsum Photos dan mengubahnya ke base64
+        $getRandomImage = function () {
+            $imageUrl = "https://picsum.photos/800/600";
+            $imageContent = @file_get_contents($imageUrl);
+            if ($imageContent === false) {
+                // Jika gagal mengambil gambar, gunakan gambar placeholder
+                return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=';
+            }
+            $base64Image = 'data:image/jpeg;base64,' . base64_encode($imageContent);
+            return $base64Image;
+        };
+
         $lessons = [
             // Aksara
             [
                 'kategori_id' => 1,
                 'judul' => 'Mengenal Aksara Thai',
                 'deskripsi' => 'Pengenalan dasar aksara Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran tentang aksara Thai...'
             ],
             [
                 'kategori_id' => 1,
                 'judul' => 'Menulis Aksara Thai',
                 'deskripsi' => 'Panduan menulis aksara Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten panduan menulis aksara Thai...'
             ],
 
@@ -35,7 +50,7 @@ class LessonSeeder extends Seeder
                 'kategori_id' => 2,
                 'judul' => 'Nada dalam Bahasa Thai',
                 'deskripsi' => '5 Nada dasar bahasa Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran tentang nada dalam bahasa Thai...'
             ],
 
@@ -44,7 +59,7 @@ class LessonSeeder extends Seeder
                 'kategori_id' => 3,
                 'judul' => 'Angka dalam Bahasa Thai',
                 'deskripsi' => 'Sistem angka Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran tentang angka Thai...'
             ],
 
@@ -53,7 +68,7 @@ class LessonSeeder extends Seeder
                 'kategori_id' => 4,
                 'judul' => 'Simbol-simbol Thai',
                 'deskripsi' => 'Pengenalan simbol umum Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran tentang simbol-simbol Thai...'
             ],
 
@@ -62,7 +77,7 @@ class LessonSeeder extends Seeder
                 'kategori_id' => 5,
                 'judul' => 'Dasar Bahasa Thai',
                 'deskripsi' => 'Pengenalan dasar bahasa Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran dasar bahasa Thai...'
             ],
 
@@ -71,7 +86,7 @@ class LessonSeeder extends Seeder
                 'kategori_id' => 6,
                 'judul' => 'Budaya Thailand',
                 'deskripsi' => 'Pengenalan budaya Thailand',
-                'gambar' => null,
+                'gambar' => $getRandomImage(),
                 'isi' => 'Konten pembelajaran tentang budaya Thailand...'
             ]
         ];
